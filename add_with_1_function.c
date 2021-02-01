5b199e09dff3d7a61f4463e27000edb01185fc02
neha-manju <75286410+neha-manju@users.noreply.github.com>
2021-02-01 16:28:20 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -4,8 +4,8 @@
 int main()
 {
   int a,b,sum;
-  printf('enter two numbers')
-    scanf('%d,%d',&a,&b)
+  printf("enter two numbers");
+    scanf("%d,%d",&a,&b);
   sum=a+b;
   printf('%d',&sum);
 }
