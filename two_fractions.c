2bf02307b9506d00c0508179ef45642ab2a57f49
neha-manju <75286410+neha-manju@users.noreply.github.com>
2021-02-15 09:03:16 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,16 @@
 //WAP to find the sum of two fractions.
+
+#include<stdio.h>
+struct sum
+{
+int x,y;
+};
+int main()
+{
+int z;
+struct sum s;
+printf("enter two numbers");
+scanf("%d%d",&s.x,&s.y);
+z=s.x+s.y;
+printf("sum of two numbers is %d",z);
+}
