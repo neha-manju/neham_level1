a876e175c8380286acb20fce860ef15567797b22
neha-manju <75286410+neha-manju@users.noreply.github.com>
2021-04-16 16:11:13 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,16 +1,51 @@
 //WAP to find the sum of two fractions.
 
 #include<stdio.h>
-struct sum
+struct fraction
 {
-int x,y;
-};
+int n,d;
+}f1,f2;
+
+fraction input(int n)
+{
+printf("enter the numerator of the first fraction");
+scanf("%d",&f1.n);
+printf("enter the denominator for the first  fraction");
+scanf("%d",&f1.d);
+printf("enter the numerator for the second fraction");
+scanf("%d",&f2.n);
+printf("enter the denominator for the second fraction");
+scanf("%d",&f2.d);
+}
+
+int gcd ( int n , int d )
+{
+int ( n == 0 )
+return d;
+return gcd ( d % n , a);
+}
+fraction evaluate(fraction ans , fraction f1 , fraction f2)
+{
+ans.n = (f1.n*f2.d)+(f2.n*f1.d);
+ans.d = f1.d*f2.d;
+int g;
+g = gcd(ans.n , ans.d);
+ans.n = ans.n/g;
+ans.d = ans.d/g;
+return ans;
+}
+
+int output ( fraction f1 , fraction f2 , fraction ans)
+{
+printf("the sum of %d / %d and %d / %d is %d / %d\n",f1.n , f1.d , f2.n , f2.d , ans.n , ans.d);
+}
+
 int main()
 {
-int z;
-struct sum s;
-printf("enter two numbers");
-scanf("%d%d",&s.x,&s.y);
-z=s.x+s.y;
-printf("sum of two numbers is %d",z);
+fraction f1 , f2 , ans;
+f1 = input()
+f2 = input();
+ans = evaluate(ans , f1 , f2);
+output( f1 , f2 , ans);
 }
+
